8a1b0de23f2667133c398608f4c15b7f2a1cf319
Lulu Nwenyi <oluchinwenyi@gmail.com>
2022-04-02 14:03:46 +0100

    v0 app for image

---
+++ docker/main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+
+load_dotenv()
+  
+@app.route('/')
+def hello():
+    return "Welcome to my SCA Cloud School Application."
+    
